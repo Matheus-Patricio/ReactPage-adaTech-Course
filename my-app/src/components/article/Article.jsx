60df9d9c40fdb38520ddf4257115fefccb07db2e
Matheus Patricio <contato.matheuspatricio@gmail.com>
2024-06-07 23:16:25 -0300

    Button component

--- my-app/src/components/article/Article.jsx
+++ my-app/src/components/article/Article.jsx
@@ -1,16 +1,31 @@
 import React from 'react'
-import jWebb from '../../assets/imgs/jameswebb.png'
 import './article_style.css'
 
 
 export class Article extends React.Component {
+    constructor(){
+        super()
+        this.state = {reader: false}
+    }
+    altState = () => {
+        this.setState({reader: !this.state.reader})
+    }
+
+
     render() {
         return (
             <article>
                  <img src={this.props.thumbnail} alt={this.props.title} />
                  <div className="articleText">
+
                     <h2>{this.props.title}</h2>
-                    <h3>{this.props.provider}</h3>
+                    <div className="secondary">
+                        <h3>{this.props.provider}</h3>
+
+                        <button
+                        onClick={this.altState}>{this.state.reader ? "Lida" : "Não Lida"}
+                        </button>
+                    </div>
                     <p>{this.props.description}</p>
                  </div>
             </article>
